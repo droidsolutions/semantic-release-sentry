76dcb94c93557b780e2f6bf119fe8e8ea7eb0981
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2019-12-20 14:25:35 +0100

    fix: rename verify to verifyConditions

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const fail = require("./lib/fail");
 const prepare = require("./lib/prepare");
 const publish = require("./lib/publish");
 const success = require("./lib/success");
-const verify = require("./lib/verify");
+const verifyConditions = require("./lib/verify");
 
-module.exports = { fail, prepare, publish, success, verify };
+module.exports = { fail, prepare, publish, success, verifyConditions };

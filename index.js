239af0003b13e10752af32cd6d16d5b8b4932057
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2019-12-20 10:48:29 +0100

    feat: initial release

---
+++ index.js
@@ -0,0 +1,7 @@
+const fail = require("./lib/fail");
+const prepare = require("./lib/prepare");
+const publish = require("./lib/publish");
+const success = require("./lib/success");
+const verify = require("./lib/verify");
+
+module.exports = { fail, prepare, publish, success, verify };
